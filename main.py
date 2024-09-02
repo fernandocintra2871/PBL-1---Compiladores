7688e599971b7db0251c5da3d8e36a7ed1ce5ad6
Lucas Gabriel <68016822+lucasxgb@users.noreply.github.com>
2024-09-02 19:34:04 -0300

    :sparkles: feat: implementação "Crua"

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print('Olá mundo')
+class Token:
+    def __init__(self, tag):
+        self.tag = tag
+
+
+class Num(Token):
+    def __int__(self, tag, valor):
+        super.__init__(self, tag)
+        self.valor = valor
+
+
+class Identifier(Token):
+    def __int__(self, tag, lexeme):
+        super.__init__(self, tag)
+        self.lexeme = lexeme
+
+
+class Lexer:
+    line = 1
+    _peek = ''
+
+    def scan(self, palavra):
+        state = 0
+        counter = 0
+        while counter != len(palavra):
+
+
+
