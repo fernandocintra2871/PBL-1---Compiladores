e884c9070f81abe4d853290fad6fb70aa3e99197
Lucas Gabriel <68016822+lucasxgb@users.noreply.github.com>
2024-09-04 16:35:50 -0300

    :sparkles: feat: atualização

--- data.py
+++ data.py
@@ -21,3 +21,36 @@ reservedWords = {
     "false", 
     "extends"
 }
+
+
+# Dicionário que contém todas as expressões regulares 
+'''
+    As outras expressões como :
+    numbers: ( - )? Digit+( . Digit+)?
+    string: "(Letter | Digit | Symbol)*"
+    character: '(Letter | Digit | Symbol)' 
+    Symbols: ASCII from 32 to 126 (except 34 and 39)
+
+    São composições que utilizam as outras expressões regulares, por exemplo.
+'''
+
+
+
+    # "identifiers": r'[A-Za-z](?:[A-Za-z0-9_]*)'
+regularExpressions = {
+    "numbers" : r"(-)?[0-9]+(\.[0-9]+)?",
+    "identifiers": r"[a-zA-Z]([a-zA-Z0-9_])*",
+    "letter" : r"[a-zA-Z]",
+    "digit" : r"[0-9]",
+    "operatorsArithmetic": ['+', '-', '++', '--','*', '/'],
+    "operatorsRelational" : ['==', '=', '!=', '>', '>=', '<', '<=', ],
+    "operatorsLogical" : ['&&', '||'],
+    "acessOperator" : ['.'],
+    "delimiters" : [';',',','(',')','{','}'],
+
+}
+
+## Algumas regras:
+    # - Se vem // seguidas é um comment
+    # - Se vem / e depois * é um comentário em bloco
+    # - Se recebe " é inicio de string, a string só encerra quando achar " na mesma linha, ou então será um erro
--- lexer.py
+++ lexer.py
@@ -14,13 +14,7 @@
 #F = conjunto de estados de aceitação {q1}
 
 import re
-from data import reservedWords
-
-_letter = r'[a-zA-Z]'
-_oneAndMoreLetter = r'(?:[A-Za-z0-9_]*)'
-_digit = r'[0-9]'
-_identifiers = r'[A-Za-z](?:[A-Za-z0-9_]*)'
-
+from data import reservedWords, regularExpressions
 
 '''
 tô em s0, recebo uma letter => s1
@@ -32,61 +26,107 @@ tô em s1, recebo diferente => s0
 estados = ["s0", "s1"]
 # alfabeto = "_value_"
 func_transictions = {}
-estadoInicial = "s0"
-estadoAtual = estadoInicial
-estadosFinais = ["s1"]
+# initState = "s0"
+# currentState = initState
+estadosFinais = ["s1, s2"]
 
 
 #line representa cada linha do arquivo, por exemplo.
-line = "extends="
-counter = 0
-estadoAtual = estadoInicial
-lexeme = ""
-currentIndex = 0
 
-# Função que verifica se está contido na lista de palavras chaves 
-def  checkInReservedWords(value):
-    print(value in reservedWords)
-    if value in reservedWords:
-        print (f'<keyword, {value}>')
+
+
+tokenList = []
+errorList = []
+
+
+
+
+# Função que verifca o tipo de lexema
+def  checkLexemeType(item):
+    print(item)
+    if item in reservedWords:
+        tokenList.append(f'<keyword, {item}>')
+
+    if item in regularExpressions['operatorsRelational'] or item in regularExpressions['operatorsLogical'] or item in regularExpressions['operatorsArithmetic']:
+        tokenList.append(f'<operators, {item}>')
+
+    if re.fullmatch(regularExpressions['numbers'], item):
+        tokenList.append(f'<number, {item}>')
      
+    
 
+line = "extends=34;"
 
+def startAnalyze():
+    initState = "s0"
+    counter = 0
+    currentState = initState
+    lexeme = ""
+    currentIndex = 0
 
-while currentIndex < len(line):
+    while currentIndex <= len(line) -1:
     # usado pra algumas verificações que serão preciso
-    next_index = currentIndex + 1 if currentIndex + 1 < len(line) else currentIndex
 
-    for char in line:
-        currentIndex += 1
-        if estadoAtual == "s0":
-            if re.fullmatch(_letter,char):
-                estadoAtual = "s1"
-                lexeme = lexeme + char
-            else:
-                estadoAtual = "s0"
-                break
-    
-        elif estadoAtual == "s1":
+        for char in line:
+            nextIndex = currentIndex + 1 if currentIndex + 1 <= len(line) -1 else currentIndex
+            if currentState == "s0":
+                if re.fullmatch(regularExpressions['letter'],char):
+                    currentState = "s1"
+                    lexeme = lexeme + char
+
+                elif re.fullmatch(regularExpressions['digit'], char):
+                    currentState = "s2"
+                    lexeme = lexeme + char
+   
+                else:
+                    currentState = "s0"
+
+            elif currentState == "s1":
+                if re.fullmatch(regularExpressions['letter'],char) or re.fullmatch(regularExpressions['digit'],char) or char == '_':
+                    currentState = "s1"
+                    lexeme = lexeme + char 
+
+                    if not re.fullmatch(regularExpressions['letter'],line[nextIndex]) and not re.fullmatch(regularExpressions['digit'],line[nextIndex]) and not line[nextIndex] == '_':
+                        currentState = "s0"   
+                        checkLexemeType(lexeme)
+                        lexeme = ''
+                else:
+                    checkLexemeType(lexeme)
+                    lexeme = ''
+
             
-            if re.fullmatch(_oneAndMoreLetter,char):
-                estadoAtual = "s1"
-                lexeme = lexeme + char               
-            else:
-                checkInReservedWords(lexeme)
-                index = len(line)
-                break
+            elif currentState == "s2":
+                if re.fullmatch(regularExpressions['digit'], char):
+                    currentState = "s2"
+                    lexeme = lexeme + char                           
+                    if not '.' == line[nextIndex] and not re.fullmatch(regularExpressions['digit'], line[nextIndex]):
+                        currentState = "s0"
+                        checkLexemeType(lexeme)
+                        lexeme = ''                   
+                    elif '.' == line[nextIndex]: 
+                        currentState = 's3'
+                    elif re.fullmatch(regularExpressions['digit'], line[nextIndex]): 
+                        currentState = 's2'
+                
+
+            elif currentState == "s3":
+                 if re.fullmatch(regularExpressions['digit'], char) or '.' == char:
+                    currentState = "s3"
+                    lexeme = lexeme + char                           
+                    if not re.fullmatch(regularExpressions['digit'], line[nextIndex]):
+                        currentState = "s0"
+                        checkLexemeType(lexeme)
+                        lexeme = ''                   
+                   
+                    else: 
+                        currentState = 's3'
+            currentIndex += 1
+            
     
 
+startAnalyze()
+for item in tokenList:
+    print(item)
 
-# while conter<len(alfabeto):
 
 
-
-#     '''
-#     tô em s0, recebo uma letter => s1
-#     tô em s1, recebo uma letter = s1
-#     tô em s1, recebo diferente => s0 
-#     '''
-
-#  if re.fullmatch(_identifiers, al):
